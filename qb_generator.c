3fd571155dd6c16fca8342e12886797de95d5596
Martin N <mr.avgp@googlemail.com>
2012-04-21 17:06:40 +0200

    Improved two little comments a bit.

--- qb_generator.c
+++ qb_generator.c
@@ -56,9 +56,9 @@ int main(int argc, char **argv) {
     p->primary_header->version = 0;
     p->primary_header->type = 1;
     p->primary_header->sh_present = 0;
-    p->primary_header->apid = 2047; //all 11 bit are 1.    
+    p->primary_header->apid = 2047; //all 11 bit are set to 1.    
     p->primary_header->seq_flags = PACKET_SEQ_STANDALONE;
-    p->primary_header->seq_count = 16383;
+    p->primary_header->seq_count = 16383; //all 14 bit are set to1.
     p->primary_header->length = 13;
     p->data = "Hello world!"; 
 
