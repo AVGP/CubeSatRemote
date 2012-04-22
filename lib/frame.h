a8cc696f7fdf81d64939042c280686058170d2be
Martin N <mr.avgp@googlemail.com>
2012-04-22 11:46:33 +0200

    Moving headers to lib subdirectory

---
+++ lib/frame.h
@@ -0,0 +1,25 @@
+#ifndef _FRAME_H
+#define _FRAME_H
+
+#pragma pack(0)
+
+typedef struct {
+    unsigned char   version:2;            //Is set to 00 (see CCSDS 202.0-B-3)
+    unsigned char   bypass_flag:1;        //If set to 1, some of the acceptance
+                                          //tests can be skipped. 
+    unsigned char   ctrl_command_flag:1;  //0 means actual data, 1 means command
+                                          // used for configuring the FARM system
+    unsigned char   spare:2;              //Reserved, set to 0
+    unsigned short  spacecraft_id:10;     //Spacecraft ID
+    unsigned char   vc_id:6;              //Virtual Channel ID
+    unsigned short  length:10;            //Length in bytes (incl. header)
+    unsigned char   seq;                  //Sequence number 
+} qb_tc_frame_header;
+
+typedef struct {
+    qb_tc_frame_header *header;
+    void *data;
+    unsigned short checksum;
+} qb_tc_frame;
+
+#endif
