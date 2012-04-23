0434b90a7a032592c17de145dc2da583551421d3
Martin N <mr.avgp@googlemail.com>
2012-04-24 01:06:45 +0200

    Adapted new library functionality to packetgen.

--- qb_generator.c
+++ qb_generator.c
@@ -35,39 +35,12 @@ int main(int argc, char **argv) {
 */
     int numPackets = atoi(argv[1]);
     int numSatellites = atoi(argv[2]);
-
-
-    qb_tc_packet *p = malloc(sizeof(qb_tc_packet));
-    p->primary_header = malloc(sizeof(qb_tc_packet_header));
-    p->primary_header->version = 0;
-    p->primary_header->type = 1;
-    p->primary_header->sh_present = 0;
-    p->primary_header->apid = 2047; //all 11 bit are set to 1.    
-    p->primary_header->seq_flags = PACKET_SEQ_STANDALONE;
-    p->primary_header->seq_count = 16383; //all 14 bit are set to1.
-    p->primary_header->length = 12; //The last (ZERO-)byte is not counted in the length field, as stated in the specs.
-    p->data = "Hello world!"; 
-
-    qb_tc_frame *f = malloc(sizeof(qb_tc_frame));
-    f->header = malloc(sizeof(qb_tc_frame_header));
-    f->header->version = 0;
-    f->header->bypass_flag = 0;
-    f->header->ctrl_command_flag = 0;
-    f->header->spare = 0;
-    f->header->spacecraft_id = 3;
-    f->header->vc_id=0;
-    f->header->length = sizeof(qb_tc_packet_header) + p->primary_header->length;
-    f->header->seq = 1;
-    f->data = p;
-
+/*
     unsigned short *spacecraft_ids = malloc(sizeof(short)*2);
     spacecraft_ids[0] = 1;
     spacecraft_ids[1] = 2;
     unsigned char validation_result = qb_validateFrame(f, &spacecraft_ids, 2);
-    printf("Is frame valid? -> %s (Err-Code: %i)\n", (validation_result == 0 ? "yes" : "no"), validation_result);
-
-    printf("DBG: Size of packet: %i\n", sizeof(*p));
-
+*/
     //Send it to the DB
     /*
     sendData(db,
@@ -82,6 +55,8 @@ int main(int argc, char **argv) {
         TRUE);
     releaseDatabase(db);
 */
+    qb_tc_packet *p = qb_data_to_packet(1, 1, (void *)"0123456789", 11);
+    printf("Size: %i\n", sizeof(*p));
     dumpPacket(p);
 
     return 0;
